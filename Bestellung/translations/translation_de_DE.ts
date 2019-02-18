c7e3a67d16d27b4913472013e316f5ae653672ed
unknown <you@example.com>
2019-02-18 10:38:03 +0100

    initial commit

---
+++ Bestellung/translations/translation_de_DE.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bin unterwegs</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bin unterwegs</translation>
+        </message>
+    </context>
+</TS>
